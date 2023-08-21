4fcffe874896d5aac45fc6a07938c3ea4829d420
kgovernor <k.gov@hotmail.com>
2023-08-21 10:08:36 -0500

    NLopt added to lib

--- 00-pkg_library.jl
+++ 00-pkg_library.jl
@@ -34,39 +34,48 @@ essential_pkgs = [
 # For working with data
 data_pkgs = [
     "CSV", # To export to csv
+    "RDatasets", # Load experimental datasets from R
+
+    # DataFrame manipulation and Queries
     "DataFrames", # For creating and manipulating DataFrames
     "DataFramesMeta", # Helpful macros for manipulating DataFrames
     "Chain", # Helpful macros for implementing macros above
-    "GLM", # Regressions
-    "FixedEffectModels", # For fixed effects regressions
     "CategoricalArrays", # To initialize catagorical data
     "ShiftedArrays", # Useful for lagging and leading the dataset
     "Query", # To query data efficiently
-    "RDatasets", # Load experimental datasets from R
+
+    # Data Analysis
+    "GLM", # Regressions
+    "FixedEffectModels", # For fixed effects regressions
 ]
 
 # For mathematical analysis
 analysis_pkgs = [
     # Interval Arithmetic
     "IntervalArithmetic", # To do math on intervals
+
     # Integration and Interpolation
     "QuadGK", # Numerical Integration
     "FastGaussQuadrature", # Numerical Integration
     "Interpolations", # For interpolating data points
     "TaylorModels" # For Taylor Series Expansion
+
     # Differentiation and Root finding
     "ForwardDiff", # Numerical Differentiation
     "Zygote", # Numerical Differentiation
     "Roots", # Root finding packages
     "IntervalRootFinding", # Root finding using intervals
+
     # Solving Systems
     "LinearSolve", # Solve system of linear equations
     "NLSolve", # Solve system of non-linear equations
+
     # Optimization
-    "JuMP", # Numerical Optimizer
-    "Ipopt", # Numerical Optimizer
+    "JuMP", # Numerical Optimizer program
+    "Ipopt", # Numerical Optimizer, use with JuMP
     "Optim", # Numerical Optimizer
-    "BlackBoxOptim", # Numerical Optimizer
+    "BlackBoxOptim", # Numerical Optimizer, good for global Optimization
+    "NLopt", # Numerical Optimizer, good for global Optimization, use with JuMP
 ] 
 
 # For building neural networks
